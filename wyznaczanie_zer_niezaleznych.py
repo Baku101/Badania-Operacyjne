0aa9ae1ce36d0f72ce85311f3beb287a5974405b
Baku101 <128596106+Baku101@users.noreply.github.com>
2024-04-17 18:47:45 +0200

    Update wyznaczanie_zer_niezaleznych.py

--- wyznaczanie_zer_niezaleznych.py
+++ wyznaczanie_zer_niezaleznych.py
@@ -8,31 +8,30 @@ macierz = [
     [1, 1, 2, 4, 0]
 ]
 
-def wyznaczanie_zer(macierz,fi):
+def wyznaczanie_zer(macierz):
 
-    rz = len(macierz)
-    kol = len(macierz[0])
+    rz = len(macierz)           #liczba rzędów macierzy
+    kol = len(macierz[0])       #liczba kolumn macierzy
 
-    for i in range(rz):
-        for j in range(kol):
-            if macierz[i][j] == 0:
-                    zero_rz = False
+    for i in range(rz):         #dla każdego rzędu
+        for j in range(kol):    #dla każdej kolumny
+            if macierz[i][j] == 0:      #jeśli element jest równy 0
+                    zero_rz = False    
                     zero_kol = False
-                    for k in range(kol):
-                        if macierz[i][k] == "0*":
-                            zero_rz = True
-                            break
-                    for k in range(rz):
-                        if macierz[k][j] == "0*":
-                            zero_kol = True
-                            break
-                    if not zero_rz and not zero_kol:
-                        macierz[i][j] = "0*"
-                    else:
-                        macierz[i][j] = "0/"
+                    for k in range(kol):        #dla każdej kolumny
+                        if macierz[i][k] == "0*":   #jeśli jest 0* w rzędzie
+                            zero_rz = True      #ustawiamy zmienną na True
+                            break               #przerywamy pętle
+                    for k in range(rz):         #dla każdego rzędu
+                        if macierz[k][j] == "0*":   #jeśli jest 0* w kolumnie 
+                            zero_kol = True         #ustawiamy zmienną na True
+                            break               #przerywamy pętle
+                    if not zero_rz and not zero_kol:    #jeśli obie zmienne są na False
+                        macierz[i][j] = "0*"        #zero jest niezależne, zamieniamy na 0*
+                    else:                       #jeśli nie
+                        macierz[i][j] = "0/"        #zero jest zależne, zamieniamy na 0/
     return macierz
 
-fi = 0
-macierz = wyznaczanie_zer(macierz,fi)
+macierz = wyznaczanie_zer(macierz)
 for rzad in macierz:
     print(rzad)
