71f121bb8d293700684be6719d64fc4647750383
ArthasToZiom <128599025+ArthasToZiom@users.noreply.github.com>
2024-04-17 21:24:05 +0200

    EKSTERMINACJA ZERRRR

--- calosc.py
+++ calosc.py
@@ -1,4 +1,3 @@
-
 def znajdx_najmniejsze_wiersze(macierz: list[list[int]]) -> list[int]:
     """
     Funkcja znajdująca najmniejszy element w każdym z wierszy macierzy.
@@ -45,7 +44,7 @@ def redukcja_macierzy(macierz: list[list[int]]) -> tuple[list[list[int]], int]:
     # znalezienie w obecnej macierzy najmniejszych elementów w wierszach
     najmniejsze_wiersze = znajdx_najmniejsze_wiersze(macierz_kopia)
 
-    # przejście po każdym wierzu i odjęcie od każdego z jego elementów najmniejszego
+    # przejście po każdym wierszu i odjęcie od każdego z jego elementów najmniejszego
     # elementu w obecnym wierszu
     for i in range(len(macierz_kopia[0])):
         for j in range(len(macierz_kopia)):
@@ -72,39 +71,89 @@ def redukcja_macierzy(macierz: list[list[int]]) -> tuple[list[list[int]], int]:
 
     return macierz_kopia, dolne_ograniczenie
 
+
 def wyznaczanie_zer(macierz):
+    rz = len(macierz)  # liczba rzędów macierzy
+    kol = len(macierz[0])  # liczba kolumn macierzy
 
-    rz = len(macierz)           #liczba rzędów macierzy
-    kol = len(macierz[0])       #liczba kolumn macierzy
-
-    for i in range(rz):         #dla każdego rzędu
-        for j in range(kol):    #dla każdej kolumny
-            if macierz[i][j] == 0:      #jeśli element jest równy 0
-                    zero_rz = False    
-                    zero_kol = False
-                    for k in range(kol):        #dla każdej kolumny
-                        if macierz[i][k] == "0*":   #jeśli jest 0* w rzędzie
-                            zero_rz = True      #ustawiamy zmienną na True
-                            break               #przerywamy pętle
-                    for k in range(rz):         #dla każdego rzędu
-                        if macierz[k][j] == "0*":   #jeśli jest 0* w kolumnie 
-                            zero_kol = True         #ustawiamy zmienną na True
-                            break               #przerywamy pętle
-                    if not zero_rz and not zero_kol:    #jeśli obie zmienne są na False
-                        macierz[i][j] = "0*"        #zero jest niezależne, zamieniamy na 0*
-                    else:                       #jeśli nie
-                        macierz[i][j] = "0/"        #zero jest zależne, zamieniamy na 0/
+    for i in range(rz):  # dla każdego rzędu
+        for j in range(kol):  # dla każdej kolumny
+            if macierz[i][j] == 0:  # jeśli element jest równy 0
+                zero_rz = False
+                zero_kol = False
+                for k in range(kol):  # d la każdej kolumny
+                    if macierz[i][k] == "0*":  # jeśli jest 0* w rzędzie
+                        zero_rz = True  # ustawiamy zmienną na True
+                        break  # przerywamy pętle
+                for k in range(rz):  # dla każdego rzędu
+                    if macierz[k][j] == "0*":  # jeśli jest 0* w kolumnie
+                        zero_kol = True  # ustawiamy zmienną na True
+                        break  # przerywamy pętle
+                if not zero_rz and not zero_kol:  # jeśli obie zmienne są na False
+                    macierz[i][j] = "0*"  # zero jest niezależne, zamieniamy na 0*
+                else:  # jeśli nie
+                    macierz[i][j] = "0/"  # zero jest zależne, zamieniamy na 0/
     return macierz
 
+def eliminacja_zer(M):
+
+    zero_niezal = "0*"
+    zero_zal = "0/"
+    rozmiar = len(M)
+    wiersz = [i for i in range(rozmiar)]
+    nowy_wiersz = [i for i in range(rozmiar)]
+    kolumna = []
+    nowa_kolumna = [i for i in range(rozmiar)]
+    linie = 0
+
+    # 1. Poszukiwanie maksymalnego skojarzenia
+    # 1a. Oznaczyć każdy wiersz nieposiadający niezależnego 0
+    for i in range(rozmiar):
+        for j in range(rozmiar):
+            if M[i][j] == zero_niezal:
+                wiersz.remove(i)
+                linie += 1
+    # 1b. Oznaczyć każdą kolumnę posiadającą zależne 0 w oznaczonym wierszu
+    for i in wiersz:
+        for j in range(rozmiar):
+            if M[i][j] == zero_zal:
+                kolumna.append(j)
+                linie += 1
+    # 1c. Oznaczyć każdy wiersz posiadający niezależne 0 w oznaczonej kolumnie
+    for i in range(rozmiar):
+        for j in kolumna:
+            if M[i][j] == zero_niezal:
+                wiersz.append(i)
+                linie += 1
+    # Wypadek, w którym liczba linii kryjących wartości jest równa liczbie zer niezależnych
+    if linie == rozmiar:
+        return "Liczba linii odpowiada wielkości macierzy"
+
+    # Usuwanie kolumn
+    M = [[rzad[i] for i in range(rozmiar) if i not in kolumna] for rzad in M]
+    # Usuwanie wierszy
+    M = [rzad for index, rzad in enumerate(M) if index in wiersz]
+
+    return M
+
+
 M = [[5, 2, 3, 2, 7],
-         [6, 8, 4, 2, 5],
-         [6, 4, 3, 7, 2],
-         [6, 9, 0, 4, 0],
-         [4, 1, 2, 4, 0]]
+     [6, 8, 4, 2, 5],
+     [6, 4, 3, 7, 2],
+     [6, 9, 0, 4, 0],
+     [4, 1, 2, 4, 0]]
+
+
 def main():
     macierz, fi = redukcja_macierzy(M)
     macierz2 = wyznaczanie_zer(macierz)
 
     for rzad in macierz2:
         print(rzad)
+    print()
+
+    macierz3 = eliminacja_zer(macierz2)
+    for rzad in macierz3:
+        print(rzad)
+
 main()
---
+++ eliminacja_zer.py
@@ -0,0 +1,58 @@
+# Algorytm wyznaczający minimalny zbiór linii wykreślających wszystkie zera z macierzy.
+# Macierz = list(list)
+def eliminacja_zer(M):
+
+    zero_niezal = "0*"
+    zero_zal = "0/"
+    rozmiar = len(M)
+    wiersz = [i for i in range(rozmiar)]
+    nowy_wiersz = [i for i in range(rozmiar)]
+    kolumna = []
+    nowa_kolumna = [i for i in range(rozmiar)]
+    linie = 0
+
+    # 1. Poszukiwanie maksymalnego skojarzenia
+    # 1a. Oznaczyć każdy wiersz nieposiadający niezależnego 0
+    for i in range(rozmiar):
+        for j in range(rozmiar):
+            if M[i][j] == zero_niezal:
+                wiersz.remove(i)
+                linie += 1
+    # 1b. Oznaczyć każdą kolumnę posiadającą zależne 0 w oznaczonym wierszu
+    for i in wiersz:
+        for j in range(rozmiar):
+            if M[i][j] == zero_zal:
+                kolumna.append(j)
+                linie += 1
+    # 1c. Oznaczyć każdy wiersz posiadający niezależne 0 w oznaczonej kolumnie
+    for i in range(rozmiar):
+        for j in kolumna:
+            if M[i][j] == zero_niezal:
+                wiersz.append(i)
+                linie += 1
+    # Wypadek, w którym liczba linii kryjących wartości jest równa liczbie zer niezależnych
+    if linie == rozmiar:
+        return "Liczba linii odpowiada wielkości macierzy"
+
+    # Usuwanie kolumn
+    M = [[rzad[i] for i in range(rozmiar) if i not in kolumna] for rzad in M]
+    # Usuwanie wierszy
+    M = [rzad for index, rzad in enumerate(M) if index in wiersz]
+
+    return M
+
+
+M = [['0*', '0/', 1, '0/', 5],
+     [1, 6, 2, '0*', 3],
+     [1, 2, 1, 5, '0*'],
+     [3, 9, '0*', 4, '0/'],
+     [1, 1, 2, 4, '0/']]
+
+
+def main():
+    macierz = eliminacja_zer(M)
+    for rzad in macierz:
+        print(rzad)
+
+
+main()
